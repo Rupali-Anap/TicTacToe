5bbcbae469ec1e15b87f7c45804c9b7cd5b959f1
Rupali Anap <anaprupali333@gmail.com>
2023-01-05 03:51:03 -0800

    Added: Showing Board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,35 +3,47 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-        static char [] board = new char[10];
-        static char playerSymbol;
-        static char computerSymbol;
+    static char[] board = new char[10];
+    static char playerSymbol;
+    static char computerSymbol;
 
-        static void createBoard() {
-            for(int i=1; i<board.length; i++) {
-                board[i] = ' ';
-            }
-            System.out.println("board is created");
+    static void createBoard() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
         }
+        System.out.println("board is created");
+    }
 
-        static void assignSymbol(){
-            Scanner sc = new Scanner(System.in);
-            System.out.println("Choose your letter (X or O): ");
-            playerSymbol = sc.next().charAt(0);
-            if(playerSymbol == 'X' || playerSymbol == 'x'){
-                computerSymbol = 'O';
-            }else if(playerSymbol == 'O' || playerSymbol == 'o'){
-                computerSymbol = 'X';
-            }else{
-                System.out.println("Invalid input");
-                assignSymbol();
-            }
+    static void assignSymbol() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Choose your letter (X or O): ");
+        playerSymbol = sc.next().charAt(0);
+        if (playerSymbol == 'X' || playerSymbol == 'x') {
+            computerSymbol = 'O';
+        } else if (playerSymbol == 'O' || playerSymbol == 'o') {
+            computerSymbol = 'X';
+        } else {
+            System.out.println("Invalid input");
+            assignSymbol();
         }
+    }
 
-        public static void main(String[] args) {
+    static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+        System.out.println(" ");
+    }
+
+
+    public static void main(String[] args) {
         System.out.println("Welcome To Tic Tac Toe Game");
+
         createBoard();
         assignSymbol();
-
+        showBoard();
     }
+
 }
